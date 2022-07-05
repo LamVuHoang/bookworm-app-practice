f0770e47958a85e8456e89423eae60aa8e2f8ce7
Vũ Hoàng Lâm <josephlam1304@gmail.com>
2022-07-05 14:47:05 +0700

    fix(view) changes in View for fitting react-router

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Welcome from './welcome';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import About from './pages/About';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-  <Welcome/>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/shop" element={<Shop /> }></Route>
+            <Route path="/product" element={<Product />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/about" element={<About />}></Route>
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById("root")
 );
